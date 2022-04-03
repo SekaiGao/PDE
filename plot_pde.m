7998773fd35d6d4b65ba39d0f200832bca178137
SekaiGao <72960984+SekaiGao@users.noreply.github.com>
2022-04-04 00:51:06 +0800

    Update plot_pde.m

--- plot_pde.m
+++ plot_pde.m
@@ -1,11 +1,16 @@
-z=readmatrix("C:\Users\SekaiGao\Desktop\&C++\pde.csv");
+%pde画图
+z=readmatrix("pde.csv");
+%M,N为空间步数与时间步数
 M=10;
 N=50;
-h=1/M;
-k=0.5/N;
-x=0:h:1;
-t=0:k:0.5;
-[X,Y]=meshgrid(x,t);
+%区间x,t
+x=[0,1];
+t=[0,0.5];
+h=(x(2)-x(1))/M;
+k=(t(2)-t(1))/N;
+x0=x(1):h:x(2);
+t0=x(1):k:t(2);
+[X,Y]=meshgrid(x0,t0);
 figure
 surf(X,Y,z,'FaceAlpha',1.0);
 
